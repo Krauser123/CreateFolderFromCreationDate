10608ead9669720d54c40848372312d7e7ea3621
Krauser123 <66463181+Krauser123@users.noreply.github.com>
2021-05-17 09:10:19 +0200

    Update Extensions.cs

--- CreateFolderFromCreationDate/Classes/Extensions.cs
+++ CreateFolderFromCreationDate/Classes/Extensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CreateFolderFromCreationDate
@@ -16,6 +16,30 @@ namespace CreateFolderFromCreationDate
                 .Select(x => x.Select(v => v.Value).ToList())
                 .ToList();
         }
+    }
 
+    public static class ExtensionDeleteToRecycleBin
+    {
+        /// <summary>  
+        /// Delete File To Recycle Bin  
+        /// WARMING: NETWORK FILES DON'T GO TO RECYCLE BIN  
+        /// </summary>  
+        /// <param name="file"></param>  
+        public static void FileRecycle(this string file)
+            =>
+        Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(file,
+            Microsoft.VisualBasic.FileIO.UIOption.OnlyErrorDialogs,
+            Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
+
+        /// <summary>  
+        /// Delete Path To Recycle Bin  
+        /// WARMING: NETWORK PATHS DON'T GO TO RECYCLE BIN  
+        /// </summary>  
+        /// <param name="path"></param>  
+        public static void DirectoryRecycle(this string path)
+            =>
+        Microsoft.VisualBasic.FileIO.FileSystem.DeleteDirectory(path,
+            Microsoft.VisualBasic.FileIO.UIOption.OnlyErrorDialogs,
+            Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
     }
 }
